8945815a146bab8429028ff3e1bd9a4583cd74f9
Gerson77 <gerson.snts55@gmail.com>
2025-05-26 16:06:40 -0300

    finalizando projeto easybank

--- main.js
+++ main.js
@@ -17,3 +17,22 @@ document.querySelectorAll(".header nav a").forEach((link) => {
     this.classList.add("active-link");
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menuHamb = document.querySelector(".menu-hamb");
+  const navMenu = document.getElementById("nav");
+
+  if (menuHamb && navMenu) {
+    menuHamb.addEventListener("click", function () {
+      navMenu.classList.toggle("active");
+      menuHamb.classList.toggle("active");
+    });
+  }
+});
+
+document.querySelectorAll('.menu a[href^="#"]').forEach((link) => {
+  link.addEventListener("click", () => {
+    document.querySelector(".menu-hamb").classList.remove("active");
+    document.querySelector(".menu").classList.remove("active");
+  });
+});
